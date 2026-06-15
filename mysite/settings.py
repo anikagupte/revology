719c113388526e83eaae01ebc140ea9fe553fffb
Anika Gupte <anikagupte@gmail.com>
2026-06-15 15:50:57 +0100

    removed debugging

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-br%o=e4@7gl=@)%7=n23l5)c@lym(degnw55)-)*cn-r^m5oox'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
